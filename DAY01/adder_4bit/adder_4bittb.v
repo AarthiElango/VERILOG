49e9b0a1c027b2c17f6b02438a95fb637a53ba03
AarthiElango <142249472+AarthiElango@users.noreply.github.com>
2025-04-10 09:01:29 +0530

    adder_4bittb.v

--- DAY01/adder_4bit/adder_4bittb.v
+++ DAY01/adder_4bit/adder_4bittb.v
@@ -1,6 +1,6 @@
 module adder_4bittb;
     reg [3:0] a, b;
-   // reg cin;
+   // reg cin
     wire [3:0] sum;
     wire cout;
 
@@ -15,11 +15,11 @@ module adder_4bittb;
     initial begin
         $monitor($time, " | a = %b | b = %b  | sum = %b | cout = %b", a, b, sum, cout);
        
-        a = 4'b0000; b = 4'b0000;
-        a = 4'b0001; b = 4'b0010; 
-        a = 4'b0101; b = 4'b0011; 
-        a = 4'b1111; b = 4'b1111; 
-        a = 4'b1111; b = 4'b1111; 
-        #10 $finish;
+           a = 4'b0000; b = 4'b0000;
+      #10  a = 4'b0001; b = 4'b0010; 
+      #10  a = 4'b0101; b = 4'b0011; 
+      #10  a = 4'b1111; b = 4'b1111; 
+      #10  a = 4'b1111; b = 4'b1111; 
+      #10  $finish;
     end
 endmodule
